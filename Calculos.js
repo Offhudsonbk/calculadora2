cd265cdac9216f7b2a224a1a12e1a0bc9389dfed
Offhudsonbk <137559748+Offhudsonbk@users.noreply.github.com>
2023-10-27 17:52:38 -0300

    calculadora2.0

--- Calculos.js
+++ Calculos.js
@@ -1,11 +1,5 @@
-function multiplica() {
-    num1 = document.getElementById("n1").value;
-    num2 = document.getElementById("n2").value;
-    document.getElementById("resultado").innerHTML = num1 * num2;
-}
-
-function divide() {
-    num1 = document.getElementById("n1").value;
-    num2 = document.getElementById("n2").value;
-    document.getElementById("resultado").innerHTML = num1 / num2;
+function calcular() {
+     peso = document.getElementById("peso").value;
+     altura = document.getElementById("altura").value;
+    document.getElementById("resultado").innerHTML =  peso * altura;
 }
